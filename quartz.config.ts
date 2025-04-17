d8c3c0c5adf93f4051bb5c735b65dff8cffc0ac5
yoondd <yoondd@kakao.com>
2025-04-17 21:35:10 +0900

    font change

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "YOON",
+    pageTitle: "👩🏻‍💻 YOON",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Pretendard Variable",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
