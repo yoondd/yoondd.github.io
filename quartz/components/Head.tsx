a3281b6bde11ef2ccbe010ac499fd0ca59733723
hye kyeong <62548969+yoondd@users.noreply.github.com>
2025-05-10 17:33:01 +0900

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -65,9 +65,9 @@ export default (() => {
 
         {!usesCustomOgImage && (
           <>
-            <meta property="og:image" content={ogImageDefaultPath} />
-            <meta property="og:image:url" content={ogImageDefaultPath} />
-            <meta name="twitter:image" content={ogImageDefaultPath} />
+            <meta property="og:image" content="https://yoondd.github.io/TIL/link-blog.jpg" />
+            <meta property="og:image:url" content="https://yoondd.github.io/TIL/link-blog.jpg" />
+            <meta name="twitter:image" content="https://yoondd.github.io/TIL/link-blog.jpg" />
             <meta
               property="og:image:type"
               content={`image/${getFileExtension(ogImageDefaultPath) ?? "png"}`}
