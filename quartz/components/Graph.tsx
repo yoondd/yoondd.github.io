7b47c41a8b676320fe67d4a9f7243ef00b4ef309
yoondd <yoondd@kakao.com>
2025-05-28 23:24:33 +0900

    add git btn

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -65,10 +65,25 @@ export default ((opts?: Partial<GraphOptions>) => {
     const globalGraph = { ...defaultOptions.globalGraph, ...opts?.globalGraph }
     return (
         <>
-          <div style={{textAlign: 'center'}}>
-            <img src="https://i.imgur.com/QXuj0ht.png" width="120" /><br/>
-            <img src="https://hits.sh/yoondd.github.io.svg?view=today+total" alt="방문자 카운터" />
+          <div style={{ textAlign: "center" }}>
+            <img src="https://i.imgur.com/QXuj0ht.png" width="120" /><br />
 
+            <div class="icons-row">
+              <a href="https://github.com/yoondd" className="icon-link" target="_blank" rel="noopener noreferrer">
+                <svg className="git-icon" viewBox="0 0 24 24" fill="currentColor">
+                  <path
+                    d="M12 0C5.374 0 0 5.373 0 12 0 17.302 3.438 21.8 8.207 23.387c.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+
+              <a href="https://gitlab.com/yoondd" className="icon-link" target="_blank" rel="noopener noreferrer">
+                <svg className="git-icon" viewBox="0 0 24 24" fill="#FC6D26">
+                  <path
+                    d="M23.955 13.587l-1.342-4.135-2.664-8.189c-.135-.416-.73-.416-.867 0L16.418 9.45H7.582L4.919 1.263c-.135-.416-.73-.416-.867 0L1.386 9.45.044 13.587a.899.899 0 00.326 1.005L12 23.054l11.63-8.462a.899.899 0 00.325-1.005z" />
+                </svg>
+              </a>
+              <img src="https://hits.sh/yoondd.github.io.svg?view=today+total" alt="방문자 카운터" />
+            </div>
           </div>
 
           <div className={classNames(displayClass, "graph")}>
@@ -77,17 +92,17 @@ export default ((opts?: Partial<GraphOptions>) => {
               <div className="graph-container" data-cfg={JSON.stringify(localGraph)}></div>
               <button className="global-graph-icon" aria-label="Global Graph">
                 <svg
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlnsXlink="http://www.w3.org/1999/xlink"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 55 55"
-                    fill="currentColor"
-                    xmlSpace="preserve"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 55 55"
+                  fill="currentColor"
+                  xmlSpace="preserve"
                 >
                   <path
-                      d="M49,0c-3.309,0-6,2.691-6,6c0,1.035,0.263,2.009,0.726,2.86l-9.829,9.829C32.542,17.634,30.846,17,29,17
+                    d="M49,0c-3.309,0-6,2.691-6,6c0,1.035,0.263,2.009,0.726,2.86l-9.829,9.829C32.542,17.634,30.846,17,29,17
                 s-3.542,0.634-4.898,1.688l-7.669-7.669C16.785,10.424,17,9.74,17,9c0-2.206-1.794-4-4-4S9,6.794,9,9s1.794,4,4,4
                 c0.74,0,1.424-0.215,2.019-0.567l7.669,7.669C21.634,21.458,21,23.154,21,25s0.634,3.542,1.688,4.897L10.024,42.562
                 C8.958,41.595,7.549,41,6,41c-3.309,0-6,2.691-6,6s2.691,6,6,6s6-2.691,6-6c0-1.035-0.263-2.009-0.726-2.86l12.829-12.829
