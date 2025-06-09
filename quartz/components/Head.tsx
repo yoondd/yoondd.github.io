5f3ced3c616a54ea1f3af727fb2bff498772c3c0
yoondd <yoondd@kakao.com>
2025-06-09 11:23:20 +0900

    vault backup: 2025-06-09 11:23:20

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -53,17 +53,18 @@ export default (() => {
                 </>
             )}
             <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous"/>
+            {/* Google tag (gtag.js) */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZV5JT44BQ2"></script>
             <script
-                dangerouslySetInnerHTML={{
-                                    __html: `
-                      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-K6RVDX3X');
-                    `,
-                }}
-            />
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-ZV5JT44BQ2');
+              `,
+                        }}
+           />
 
             <meta property="og:type" content="website"/>
             <meta property="og:url" content="https://yoondd.github.io/"/>
